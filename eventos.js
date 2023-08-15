261617efef0ce435860ff465d15b67c678da6bd3
Marina <garatmarina@gmail.com>
2023-08-14 23:49:15 -0300

    solcion para el click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,9 @@
 document.getElementById("boton").addEventListener("click", () => 
-alert("Hola! Soy el div")
+    alert("Hola! Soy el div")
 );
+
+
+document.getElementById("hola").addEventListener("click", (evento) => {
+    alert("Hola!")
+    evento.stopPropagation();
+});
